5fd036534a39502752977a43bc72c3586efe0fe5
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-02 21:03:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["o", " ", " ", " ", "x", " ", " ", " ", " "]
+board = ["o", "x", " ", " ", "x", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0]} |   |   "
+  puts " #{board[0]} | #{board[1]} |   "
   puts "-----------"
   puts "   | #{board[4]} |   "
   puts "-----------"
